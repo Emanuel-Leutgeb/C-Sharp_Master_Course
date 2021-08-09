024a4d5cba265759fe50db681c9c5dd0c158f9d5
emanuel leutgeb <emanuel@leutgeb.com>
2021-08-09 19:07:17 +0200

    Modul12

--- C-Sharp_Masterkurs/12 Modul 12_Abstrakte Klassen und Methoden/00 Program Abstrakte Klassen und Methoden.cs
+++ C-Sharp_Masterkurs/12 Modul 12_Abstrakte Klassen und Methoden/00 Program Abstrakte Klassen und Methoden.cs
@@ -4,16 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace C_Sharp_Masterkurs.Modul12_AbstrakteKlassenUndMethoden
 {
-    public class Program_AbstrakteKlassenUndMethoden
+    class Program_AbstrakteKlassenUndMethoden
     {
+
         public Program_AbstrakteKlassenUndMethoden()
         {
+
         }
         public void Modul12()
         {
-            
+            Mechatroniker dominik = new Mechatroniker("Dominik", 2500);
+            dominik.PrintInformation();
+            dominik.ArbeitVerrichten();
         }
     }
+    
 }
--- C-Sharp_Masterkurs/12 Modul 12_Abstrakte Klassen und Methoden/01 Abstrakte Klassen.cs
+++ C-Sharp_Masterkurs/12 Modul 12_Abstrakte Klassen und Methoden/01 Abstrakte Klassen.cs
@@ -1,10 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace C_Sharp_Masterkurs.Modul12_AbstrakteKlassenundMethoden
 {
-    public class AbstrakteKlassen
+    abstract class Arbeiter
     {
-        public AbstrakteKlassen()
+        public string Name { get; set; }
+        public decimal Gehalt { get; set; }
+
+        public Arbeiter(string name, decimal gehalt)
         {
+            Name = name;
+            Gehalt = gehalt;
         }
+
+        public void PrintInformation()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Gehalt: " + Gehalt);
+        }
+
+        public abstract void ArbeitVerrichten();
     }
 }
